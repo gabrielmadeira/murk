a0312216c0b196a26f59f6ea373a13ac14291870
henriquebm <pokeybm10@hotmail.com>
2022-08-23 01:36:38 -0300

    Board is sizeable

--- Assets/GroundScrip.cs
+++ Assets/GroundScrip.cs
@@ -7,7 +7,7 @@ public class GroundScrip : MonoBehaviour
     public GameObject wallPrefab;
 
     private float scale_x;
-    private float scale_y;
+    private float scale_z;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +25,14 @@ public class GroundScrip : MonoBehaviour
     void BuildWalls()
     {
         scale_x = 10*transform.localScale.x;
-        scale_y = 10*transform.localScale.y;
+        scale_z = 10*transform.localScale.z;
 
-        wallPrefab.transform.localScale = new Vector3(scale_x, 4, 2);
-
+        wallPrefab.transform.localScale = new Vector3(scale_z, 4, 2);
         Instantiate(wallPrefab, new Vector3(scale_x/2, 1.98f, 0), Quaternion.Euler(0, 90, 0));
         Instantiate(wallPrefab, new Vector3(-scale_x/2, 1.98f, 0), Quaternion.Euler(0, 90, 0));
 
-        Instantiate(wallPrefab, new Vector3(0, 1.98f, scale_x/2), Quaternion.Euler(0, 0, 0));
-        Instantiate(wallPrefab, new Vector3(0, 1.98f, -scale_x/2), Quaternion.Euler(0, 0, 0));
-
-
+        wallPrefab.transform.localScale = new Vector3(scale_x, 4, 2);
+        Instantiate(wallPrefab, new Vector3(0, 1.98f, scale_z/2), Quaternion.Euler(0, 0, 0));
+        Instantiate(wallPrefab, new Vector3(0, 1.98f, -scale_z/2), Quaternion.Euler(0, 0, 0));
     }
 }

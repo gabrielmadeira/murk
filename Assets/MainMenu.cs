f416af3c247fadbcf3f4f0065ba0cfe43e15bf2d
henriquebm <pokeybm10@hotmail.com>
2022-08-30 12:23:35 -0300

    audio guided menu prototype

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,8 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.EventSystems;
+
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource voiceAudioSrc;
+    public AudioClip welcomeAudio;
+    public AudioClip menuAudio;
+    public AudioClip startSelectedAudio;
+    public AudioClip settingsSelectedAudio;
+    public AudioClip quitSelectedAudio;
+    public AudioClip SpacebarTopAudio; //We are missing this one
+
+    GameObject currentSelected;
+    GameObject previouslySelected;
+
+    public static bool welcomed = false;
+    public static bool uninterruptable = false;
+
+    public float secondsUntilInstuctions = 0;
+
+    void Start() {
+        if (!welcomed)
+        {
+            PlayAudioClip(welcomeAudio);
+            uninterruptable = true;
+            welcomed = true;
+        }
+    }
+ 
+     void Update()
+     {
+        secondsUntilInstuctions -= Time.deltaTime; // Counts down seconds until next instruction
+        if (secondsUntilInstuctions<=0 && !voiceAudioSrc.isPlaying) // Checks if it can play instructions right now
+        {
+            PlayAudioClip(menuAudio);
+            uninterruptable = true;
+            secondsUntilInstuctions = 4*60; // Keeps silent for 4 minutes and then gives instructions agains
+        }
+
+        if(uninterruptable && !voiceAudioSrc.isPlaying) // Turns off uninterruptable bool if the audio turned off
+            uninterruptable = false;
+
+        if (!uninterruptable) {
+            currentSelected = EventSystem.current.currentSelectedGameObject;
+            if (!previouslySelected || (previouslySelected.name != currentSelected.name))
+            {
+                if(currentSelected.name == "PlayButton")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    PlayAudioClip(startSelectedAudio);
+                }
+                else if(currentSelected.name == "SettingsButton")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    PlayAudioClip(settingsSelectedAudio);
+                }
+                else if(currentSelected.name == "QuitButton")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    PlayAudioClip(quitSelectedAudio);
+                }
+                print(currentSelected.name);
+            }
+        }
+        
+     }
+
     public void PlayGame() {
         SceneManager.LoadScene(1);
     }
@@ -15,5 +80,11 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    void PlayAudioClip(AudioClip soundClip)
+    {
+        voiceAudioSrc.clip = soundClip;
+        voiceAudioSrc.Play();
     }
 }
--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -5,8 +5,23 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+using UnityEngine.EventSystems;
+
 public class OptionsMenu : MonoBehaviour
 {
+    public AudioSource voiceAudioSrc;
+    public AudioClip enterSettingsAudio;
+    public AudioClip quitSettingsAudio;
+    public AudioClip mapSizeSelectedAudio;
+    public AudioClip mapSizeChangedAudio;
+    public AudioClip debugModeTurnOffAudio;
+    public AudioClip debugModeTurnOnAudio;
+    public AudioClip debugModeTurnedOffAudio;
+    public AudioClip debugModeTurnedOnAudio;
+
+    GameObject currentSelected;
+    GameObject previouslySelected;
+
     public static bool isDebugMode;
 
     public static float mapSizeX;
@@ -22,13 +37,53 @@ public class OptionsMenu : MonoBehaviour
         SizeSetter.text = (mapSizeX/5).ToString();
 
         DebugToggler.isOn = isDebugMode;
+
+        PlayAudioClip(enterSettingsAudio);
+        MainMenu.uninterruptable = true;
+    }
+
+    void Update() {
+        if(MainMenu.uninterruptable && !voiceAudioSrc.isPlaying) // Turns off uninterruptable bool if the audio turned off
+            MainMenu.uninterruptable = false;
+
+        if (!MainMenu.uninterruptable) {
+            currentSelected = EventSystem.current.currentSelectedGameObject;
+            if (!previouslySelected || (previouslySelected.name != currentSelected.name))
+            {
+                if(currentSelected.name == "BackToMenuButton")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    PlayAudioClip(quitSettingsAudio);
+                    print(currentSelected.name);
+                }
+                else if(currentSelected.name == "DebugToggle")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    if (isDebugMode)
+                    {
+                        PlayAudioClip(debugModeTurnOffAudio);
+                    }
+                    else
+                    {
+                        PlayAudioClip(debugModeTurnOnAudio);
+                    }
+                    print(currentSelected.name);
+                }
+                else if(currentSelected.name == "InputField (TMP)")
+                {
+                    previouslySelected = EventSystem.current.currentSelectedGameObject;
+                    PlayAudioClip(mapSizeSelectedAudio);
+                    print(currentSelected.name);
+                }
+            }
+        }
     }
 
     public void MapSize(string sizeText) {
         if (!int.TryParse(sizeText, out int size))
         {
             size = 5;
-            SizeSetter.text = "25";
+            SizeSetter.text = "5";
         }
         else {
             size = Mathf.Max(Mathf.Min(size,10),1); // Adjusts the map size from the minumum to the maximum
@@ -37,14 +92,34 @@ public class OptionsMenu : MonoBehaviour
 
         mapSizeX = size*5;
         mapSizeZ = size*5;
+
+        PlayAudioClip(mapSizeChangedAudio);
     }
 
     public void DebugMode(bool tog) {
         isDebugMode = tog;
+        if (!MainMenu.uninterruptable)
+        {
+            if (isDebugMode)
+            {
+                PlayAudioClip(debugModeTurnedOnAudio);
+            }
+            else
+            {
+                PlayAudioClip(debugModeTurnedOffAudio);
+            }
+        }
+        
         print(isDebugMode);
     }
 
     public void QuitGame() {
         SceneManager.LoadScene(0);
+    }
+
+    void PlayAudioClip(AudioClip soundClip)
+    {
+        voiceAudioSrc.clip = soundClip;
+        voiceAudioSrc.Play();
     }
 }
